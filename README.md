94dd0c1f965afd7711e69e407777f26d7460ea8d
mikebcbc <mikebconstantino@gmail.com>
2016-02-22 22:07:18 -0500

    First commit/contact bar

---
+++ README.md
@@ -0,0 +1,13 @@
+# Mike Constantino Portfolio Redesign
+
+## About
+
+This is my redesined portfolio site. Whilst the first was uncompleted, it was much more of a test rather than anything else.
+
+## Tech
+
+This site is using **SASS, Node.JS, Gulp, BrowserSync, AutoPrefixer** and **always a mobile-first approach.**
+
+## Questions
+
+Feel free to [shoot me an email](mailto:mikebconstantino@gmail.com) with questions, information, or just to say hi!
---
+++ gulpfile.js
@@ -0,0 +1,67 @@
+var gulp        = require('gulp');
+var browserSync = require('browser-sync');
+var sass        = require('gulp-sass');
+var prefix      = require('gulp-autoprefixer');
+var cp          = require('child_process');
+
+
+
+/**
+ * Refresh with BrowserSync
+ */
+gulp.task('browser-refresh', function () {
+    browserSync.reload();
+});
+
+
+
+
+/**
+ * Run browsersyc w/ sass.
+ */
+gulp.task('browser-sync', ['sass'], function() {
+    browserSync({
+        server: {
+            baseDir: ''
+        },
+        notify: false
+    });
+});
+
+
+
+
+/**
+ * Compile files from _scss into both _site/css (for live injecting) and site (for future jekyll builds)
+ */
+gulp.task('sass', function () {
+    return gulp.src('assets/css/main.scss')
+        .pipe(sass({
+            includePaths: ['css'],
+            onError: browserSync.notify
+        }))
+        .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
+        .pipe(gulp.dest('_site/assets/css'))
+        .pipe(browserSync.reload({stream:true}))
+        .pipe(gulp.dest('assets/css'));
+});
+
+
+/**
+ * Watch scss files for changes & recompile
+ * Watch html/md files and JS files. And CSS!! :)
+ */
+gulp.task('watch', function () {
+    gulp.watch('assets/css/**', ['sass']);
+    gulp.watch(['index.html'], ['browser-refresh']);
+    gulp.watch(['assets/js/**'], ['browser-refresh']);
+});
+
+
+
+
+/**
+ * Default task, running just `gulp` will compile the sass,
+ * compile the jekyll site, launch BrowserSync & watch files.
+ */
+gulp.task('default', ['browser-sync', 'watch']);
