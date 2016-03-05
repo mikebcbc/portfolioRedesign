4d6e98bf9b6effdedbeed3ea7aff3b6a03ef09a1
mikebcbc <mikebconstantino@gmail.com>
2016-03-04 23:31:30 -0500

    removed gulp weirdly putting css in _site folder

--- gulpfile.js
+++ gulpfile.js
@@ -41,7 +41,6 @@ gulp.task('sass', function () {
             onError: browserSync.notify
         }))
         .pipe(prefix(['last 15 versions', '> 1%', 'ie 8', 'ie 7'], { cascade: true }))
-        .pipe(gulp.dest('_site/assets/css'))
         .pipe(browserSync.reload({stream:true}))
         .pipe(gulp.dest('assets/css'));
 });
