a9a7498ce5d7624a23609dcb9a0cc1074b9759b1
mikebcbc <mikebconstantino@gmail.com>
2016-03-05 20:13:04 -0500

    Nav animations, finishing touches, SEO

--- assets/js/custom-animations.js
+++ assets/js/custom-animations.js
@@ -1,10 +1,47 @@
+;(function($, win) {
+  $.fn.inViewport = function(cb) {
+     return this.each(function(i,el){
+       function visPx(){
+         var H = $(this).height(),
+             r = el.getBoundingClientRect(), t=r.top, b=r.bottom;
+         return cb.call(el, Math.max(0, t>0? H-t : (b<H?b:H)));  
+       } visPx();
+       $(win).on("resize scroll", visPx);
+     });
+  };
+}(jQuery, window));
+
+function scrollNav() {
+  $('menu a').click(function(){  
+  	$('#nav-menu').trigger('click');
+    $('html, body').stop().animate({
+        scrollTop: $( $(this).attr('href') ).offset().top - 0
+    }, 1000);
+    return false;
+  });
+  $('.scrollTop a').scrollTop();
+}
+scrollNav();
+
+
 $(document).ready(function(){
 	$('#nav-menu').click(function(){
 		$(this).toggleClass('open');
+		$('html').toggleClass('menu-active');
 	});
+
+	// $('.menu-item').click(function(){
+	// 	$('#nav-menu').trigger('click');
+	// });
+
 	$('.contact-cta').click(function() {
 		$('.contact-bar').toggleClass("contact-bar-open");
 		$('.contact-info').toggleClass("contact-info-open");
 		$('.contact-cta').toggleClass("contact-cta-open");
 	});
+
+	$(".skill-list").inViewport(function(px){
+	    if(px) $(this).addClass("animating-bar") ;
+	});
+
 });
